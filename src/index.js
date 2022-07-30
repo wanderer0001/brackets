2aaaa10f91331a8dfb8641edca6b11b7bb0c7f1e
Murad <mailprostoimail@gmail.com>
2022-07-30 16:20:39 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  function isBracketsOk(str) {
+    if (str.length % 2) return false;
+
+    const stack = [];
+
+    for (const currentSymbol of str) {
+      for (const [first, second] of bracketsConfig) {
+        const topElement = stack[stack.length - 1];
+
+        if(currentSymbol === second && topElement === first) {
+          stack.pop();
+        } else if(currentSymbol === first) {
+          stack.push(currentSymbol);
+        }
+      }
+    }
+
+    return stack.length === 0;
+  }
+
+  return isBracketsOk(str);
 }
+
+
